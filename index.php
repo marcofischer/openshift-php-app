fa21802d66dade3c41651040e10e1b6434db1c11
Marco Fischer <mfischer@Marcos-MacBook-Pro.fritz.box>
2016-03-01 00:13:53 +0100

    added build name

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require "vendor/autoload.php";
 $app = new Silex\Application();
 
 $app->get('/', function() use($app) {
-    return 'Hello OpenShift!';
+    return 'Hello OpenShift! Running imge ' . getenv('OPENSHIFT_BUILD_NAME');
 });
 
 $app->get('/env', function() use($app) {
