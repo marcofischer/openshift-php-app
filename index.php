5a082b74b505d005042ab36f493a36e6edf32d8b
Marco Fischer <mfischer@Marcos-MacBook-Pro.fritz.box>
2016-03-01 02:44:28 +0100

    added index page

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ require "vendor/autoload.php";
 $app = new Silex\Application();
 
 $app->get('/', function() use($app) {
-    return 'Hello OpenShift! Running imge ' . getenv('OPENSHIFT_BUILD_NAME');
+    return '<h1>Hello OpenShift!</h1><pre>' .
+           'Running image ' . getenv('OPENSHIFT_BUILD_NAME') . '<br>' .
+           'on Container ' . getenv('HOSTNAME') . '<br>' .
+           'git repo ' . getenv('OPENSHIFT_BUILD_SOURCE') .
+           '</pre>';
 });
 
 $app->get('/env', function() use($app) {
